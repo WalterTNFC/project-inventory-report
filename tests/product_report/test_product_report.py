1aec688e1ae69ec943f49f776ee31f8c0a66de91
WalterTNFC <walter.tad@hotmail.com>
2023-01-17 18:13:39 -0300

    verify Req 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=1,
+        nome_do_produto="farinha",
+        nome_da_empresa="Nature Coffees",
+        data_de_fabricacao="2021-05-01",
+        data_de_validade="2023-06-02",
+        numero_de_serie="Farinini",
+        instrucoes_de_armazenamento="ao abrigo de luz",
+    )
+
+    assert (isinstance(str(product), str)) is True
+    assert (str(product)) == (
+        "O produto farinha fabricado em 01-05-2021 por Farinini "
+        "com validade até 02-06-2023 precisa ser armazenado "
+        "ao abrigo de luz."
+    )
