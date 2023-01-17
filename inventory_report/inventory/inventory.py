f1394fb14223a89f3b60ee429a590f78ea82dc1b
WalterTNFC <walter.tad@hotmail.com>
2023-01-17 17:52:09 -0300

    Verify Req 6

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 import csv
 import json
+import xmltodict
 
 
 class Inventory:
@@ -22,12 +23,25 @@ class Inventory:
             elif type == 'completo':
                 return CompleteReport.generate(data)
 
+    def xml_file(path, type):
+        with open(path) as file:
+            # Referência 1
+            data = xmltodict.parse(file.read())['dataset']['record']
+            if type == 'simples':
+                return SimpleReport.generate(data)
+            elif type == 'completo':
+                return CompleteReport.generate(data)
+
     @staticmethod
     def import_data(path, type):
-        try:
-            if 'csv' in path:
-                return Inventory.csv_file(path, type)
-            elif 'json' in path:
-                return Inventory.json_file(path, type)
-        except OSError:
-            raise ValueError('Type invalid')
+        if 'csv' in path:
+            return Inventory.csv_file(path, type)
+        elif 'json' in path:
+            return Inventory.json_file(path, type)
+        elif 'xml' in path:
+            return Inventory.xml_file(path, type)
+        else:
+            raise ValueError('Type Invalid')
+
+# Referências:
+# 1- https://stackabuse.com/reading-and-writing-xml-files-in-python/
