ecda532769301f87f8c026e7e59ebd16b3d1acca
WalterTNFC <walter.tad@hotmail.com>
2023-01-17 21:39:50 -0300

    verify req 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,25 +1,29 @@
 from inventory_report.inventory.product import Product
 
 
-id = 0
-nome_do_produto = "farinha",
-nome_da_empresa = "Nature Coffees",
-data_de_fabricacao = "01/05/2021"
-data_de_validade = "02/06/2023"
-numero_de_serie = "111111"
-instrucoes_de_armazenamento = "ao abrigo de luz"
-
-
 def test_relatorio_produto():
+    list = [
+        "farinha",
+        "Nature Coffees",
+        "01/05/2021",
+        "02/06/2023"
+        "Armazenar ao abrigo de luz",
+    ]
+
     product = Product(
-        id,
-        nome_do_produto,
-        nome_da_empresa,
-        data_de_fabricacao,
-        data_de_validade,
-        numero_de_serie,
-        instrucoes_de_armazenamento,
+        id=1,
+        nome_do_produto=list[0],
+        nome_da_empresa=list[1],
+        data_de_fabricacao=list[2],
+        data_de_validade=list[3],
+        numero_de_serie='111111111',
+        instrucoes_de_armazenamento=list[4],
     )
 
-    for value in product:
-        assert (value in product.__repr__()) is True
+    assert product.__repr__() == (
+        f"O produto {product['nome_do_produto']}"
+        f" fabricado em {product['data_de_fabricacao']}"
+        f" por {product['nome_da_empresa']} com validade"
+        f" até {product['data_de_validade']}"
+        f" precisa ser armazenado {product['instrucoes_de_armazenamento']}."
+    )
