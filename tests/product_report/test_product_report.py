86925ce741ef4576a5a9e5af24d38558dfe693ff
WalterTNFC <walter.tad@hotmail.com>
2023-01-17 21:12:55 -0300

    Verify Req 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,21 +1,25 @@
 from inventory_report.inventory.product import Product
 
 
+id = 0
+nome_do_produto = "farinha",
+nome_da_empresa = "Nature Coffees",
+data_de_fabricacao = "01/05/2021"
+data_de_validade = "02/06/2023"
+numero_de_serie = "111111"
+instrucoes_de_armazenamento = "ao abrigo de luz"
+
+
 def test_relatorio_produto():
     product = Product(
-        id=1,
-        nome_do_produto="farinha",
-        nome_da_empresa="Nature Coffees",
-        data_de_fabricacao="2021-05-01",
-        data_de_validade="2023-06-02",
-        numero_de_serie="Farinini",
-        instrucoes_de_armazenamento="ao abrigo de luz",
+        id,
+        nome_do_produto,
+        nome_da_empresa,
+        data_de_fabricacao,
+        data_de_validade,
+        numero_de_serie,
+        instrucoes_de_armazenamento,
     )
 
-    convert_to_string = str(product)
-    assert (isinstance(convert_to_string, str)) is True
-    assert (convert_to_string) == (
-        "O produto farinha fabricado em 01-05-2021 por Farinini "
-        "com validade até 02-06-2023 precisa ser armazenado "
-        "ao abrigo de luz."
-    )
+    for value in product:
+        assert (value in product.__repr__()) is True
