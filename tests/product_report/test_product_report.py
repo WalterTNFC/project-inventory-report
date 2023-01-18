0aaf09638e71b734c93b0826bd3ceb4b43db8127
WalterTNFC <walter.tad@hotmail.com>
2023-01-17 21:57:59 -0300

    Verify- Req 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,29 +1,42 @@
 from inventory_report.inventory.product import Product
 
+def create_product():
+    return {
+        "id": 1,
+        "nome_do_produto": "farinha",
+        "nome_da_empresa": "Nature Coffees",
+        "data_de_fabricacao": "01/05/2021",
+        "data_de_validade": "02/06/2023",
+        "numero_de_serie": "111111111",
+        "instrucoes_de_armazenamento": "Armazenar ao abrigo de luz",
+    }
+
 
 def test_relatorio_produto():
-    list = [
-        "farinha",
-        "Nature Coffees",
-        "01/05/2021",
-        "02/06/2023"
-        "Armazenar ao abrigo de luz",
-    ]
+    # list = [
+    #     "farinha",
+    #     "Nature Coffees",
+    #     "01/05/2021",
+    #     "02/06/2023"
+    #     "Armazenar ao abrigo de luz",
+    # ]
+
+    mock_product = create_product()
 
     product = Product(
-        id=1,
-        nome_do_produto=list[0],
-        nome_da_empresa=list[1],
-        data_de_fabricacao=list[2],
-        data_de_validade=list[3],
-        numero_de_serie='111111111',
-        instrucoes_de_armazenamento=list[4],
+        mock_product['id'],
+        mock_product['nome_do_produto'],
+        mock_product['nome_da_empresa'],
+        mock_product['data_de_fabricacao'],
+        mock_product['data_de_validade'],
+        mock_product['numero_de_serie'],
+        mock_product['instrucoes_de_armazenamento'],
     )
 
     assert product.__repr__() == (
-        f"O produto {product['nome_do_produto']}"
-        f" fabricado em {product['data_de_fabricacao']}"
-        f" por {product['nome_da_empresa']} com validade"
-        f" até {product['data_de_validade']}"
-        f" precisa ser armazenado {product['instrucoes_de_armazenamento']}."
+        f"O produto {mock_product['nome_do_produto']}"
+        f" fabricado em {mock_product['data_de_fabricacao']}"
+        f" por {mock_product['nome_da_empresa']} com validade"
+        f" até {mock_product['data_de_validade']}"
+        f" precisa ser armazenado {mock_product['instrucoes_de_armazenamento']}."
     )
